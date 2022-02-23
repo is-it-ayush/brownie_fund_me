d70de4983c9bfad3035d34c076b1f52bbfc5d7e0
Ayush Gupta <ayush.keshav2004@gmail.com>
2022-02-23 09:30:11 +0530

    Added comments to fund_and_withdraw.py

--- scripts/fund_and_withdraw.py
+++ scripts/fund_and_withdraw.py
@@ -1,20 +1,28 @@
-import imp
 from brownie import FundMe, accounts
 from .helpful_scripts import get_account
 
 def fund():
+    # To get the latest deployed contract. (for latest entrace fee)
     fund_me = FundMe[-1]
+    # Geting the account (depending on network)
     account = get_account()
+    # Getting the entrance fee from within the contract.
     entrance_fee = fund_me.getEntranceFee()
+    # Writing Info on Console
     print(f'[Info] The Current Entry Fee is {entrance_fee}')
     print("[Info] Funding!")
+    # Funding from account (which called) with entrance fee.
     fund_me.fund({"from":account, "value": entrance_fee})
 
 def withdraw():
+    # Getting the latest deployed contract (for latest entrace fee)
     fund_me = FundMe[-1]
+    # Getting the account. (depending on network)
     account = get_account()
+    # Calling the withdraw function in the contract.
     fund_me.withdraw({"from":account})
 
 def main():
+    # Calling both fund and withdraw.
     fund()
     withdraw()
